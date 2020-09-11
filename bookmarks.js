8a43b74a05815b4eb31177f925fd5efaaed7c78c
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2020-09-10 21:24:14 -0700

    Add files via upload
    initial

---
+++ bookmarks.js
@@ -0,0 +1,34 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Prod",
+    links: [
+      { name: "drive", url: "https://drive.google.com" },
+      { name: "mega", url: "https://mega.nz/fmLJIWDQRS" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "youtube", url: "https://youtube.com/feed/subscriptions" },
+      { name: "manga", url: "https://manga.madokami.al" },
+      { name: "mal", url: "https://myanimelist.net" },
+      { name: "rym", url: "https://rateyourmusic.com" },
+    ],
+  },
+  {
+    title: "dnd",
+    links: [
+      { name: "5e.tools", url: "https://5e.tools/5etools.html" },
+      { name: "drive", url: "https://drive.google.com/drive/folders/140YWggIhZC5--V1Df05f-ngOFQiUZfB6" },
+      { name: "r/d100", url: "https://reddit.com/r/d100" },
+      { name: "1d4chan", url: "https://1d4chan.org" },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+];
